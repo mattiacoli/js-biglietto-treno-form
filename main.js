991943a1a371d313b7e1a27a1fef745efdc67239
mattiacoli <mattia.coli@aol.com>
2025-02-19 16:57:45 +0100

    add inner html for print in page ticket information

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
 // get variable from HTML
+const formEl = document.getElementById('ticket-form')
 const distanceEl = document.getElementById('distance')
 const ageEl = document.getElementById('age')
 const buttonEl = document.querySelector('button')
 
-console.log(distanceEl, ageEl, buttonEl);
 
+// select target element
+const targetDistance = document.querySelector('.target .distance')
+const targetAge = document.querySelector('.target .age')
+const targetPrice = document.querySelector('.target .price')
 
 
 
 
 // add event listener to button for print result in console
-buttonEl.addEventListener('click', function () {
-  console.log(distanceEl.value, ageEl.value);
-  
+formEl.addEventListener('submit', function (e) {
+  e.preventDefault()
   // get variable for price and discounts to be applied
   const price = distanceEl.value * 0.21
   const discountUnder = price * 0.2
   const discontOver = price * 0.4
 
-  if(ageEl.value < 18){
-    console.log(discountUnder.toFixed(2));
-    
-  } else if (ageEl.value > 65){
-    console.log(discontOver.toFixed(2));    
+  targetDistance.innerHTML = distanceEl.value
+  targetAge.innerHTML = ageEl.value
+
+  if (ageEl.value < 18) {    
+    targetPrice.innerHTML = discountUnder.toFixed(2);
+
+  } else if (ageEl.value > 65) {    
+    targetPrice.innerHTML = discontOver.toFixed(2);
   } else {
-    console.log(price.toFixed(2));    
+    
+    targetPrice.innerHTML = price.toFixed(2);
   }
-  
+
 })
 
